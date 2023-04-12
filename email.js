52d92254f4c042ecc9ab7843672c2b478260f905
Ajith-FSD <126141461+AJITH-4@users.noreply.github.com>
2023-04-12 11:34:10 +0530

    Add files via upload

---
+++ email.js
@@ -0,0 +1,13 @@
+function foo(){
+    let resu=document.getElementById("Firstname").value;
+    console.log(resu);  
+    let resul=document.getElementById("Middlename").value;
+    console.log(resul);
+    let result=document.getElementById("Lastname").value;
+    console.log(result);
+    let res=document.getElementById("mail").value;
+    console.log(res);
+    let res1=document.getElementById("Address").value;
+    console.log(res1);
+}
+
